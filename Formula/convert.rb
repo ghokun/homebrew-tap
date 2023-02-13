25d0b639f3ea5768325a3d8d14bee2772c709e0c
jreleaserbot <jreleaser@kordamp.org>
2023-02-13 00:38:31 +0000

    convert v0.0.1

--- Formula/convert.rb
+++ Formula/convert.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.4.0 at 2023-02-13T00:08:45.449037587Z
+# Generated with JReleaser 1.4.0 at 2023-02-13T00:38:18.996602779Z
 class Convert < Formula
   desc "Convert your data files"
   homepage "https://github.com/ghokun/convert"
@@ -7,19 +7,19 @@ class Convert < Formula
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/ghokun/convert/releases/download/v0.0.1/convert-0.0.1-linux-aarch64.zip"
-    sha256 "24ae3a8276c718d455f72683f0e45fc70d4b22be3014d0b67c97293ee41de1c1"
+    sha256 "798e9a9d8c6797c0470f69be53a6cd26c95fcd9f06524d7a9abcac569d01289d"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/ghokun/convert/releases/download/v0.0.1/convert-0.0.1-linux-x86_64.zip"
-    sha256 "025ed8a0915fdec0f2ae48350f82db68175cfc7033abbd342c1262d762bb3fdf"
+    sha256 "d953fcec59c0a06f8baa8f49bc0a960c54b9789e56ec5894710e4554efbdda39"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/ghokun/convert/releases/download/v0.0.1/convert-0.0.1-darwin-aarch64.zip"
-    sha256 "cc1a60f61f6a5451ea05f8df7f6cf6c816c41528f2f2b89aad8c87a10612f77c"
+    sha256 "f15c5c70f57302489121e334a0784ae57932759f95cb63d1df3dfd021763ed8e"
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/ghokun/convert/releases/download/v0.0.1/convert-0.0.1-darwin-x86_64.zip"
-    sha256 "0ca935c14f304bebbdeac006b3c70208e7419992a94de1bbcf1a69a2501e8a7c"
+    sha256 "907d8c4ee3caee3f6e0bf96eb32564c0df0b76e3094fe5b5252fbc6a117f8402"
   end
 
 
