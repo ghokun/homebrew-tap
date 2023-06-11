80326cf9d67c20fd34e447f5a02102ce00118efb
jreleaserbot <jreleaser@kordamp.org>
2023-06-11 13:15:23 +0000

    convert v0.1.0

--- Formula/convert.rb
+++ Formula/convert.rb
@@ -1,25 +1,25 @@
-# Generated with JReleaser 1.4.0 at 2023-02-13T22:39:55.668485249Z
+# Generated with JReleaser 1.6.0 at 2023-06-11T13:15:12.09825915Z
 class Convert < Formula
   desc "Convert your data files"
   homepage "https://github.com/ghokun/convert"
-  version "0.0.2"
+  version "0.1.0"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ghokun/convert/releases/download/v0.0.2/convert-0.0.2-linux-aarch64.zip"
-    sha256 "f9109f9001ad19f2fc83a7dfd9daf81e476ad5995b0871081350ab72df483764"
+    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-linux-aarch64.zip"
+    sha256 "a406920eac20c92502a4a2b794f09dd946e6c555f866cf0144e046be443ec748"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ghokun/convert/releases/download/v0.0.2/convert-0.0.2-linux-x86_64.zip"
-    sha256 "472d3e47985ebd96018a8a6319280a28e2b477ae3960385e88aab9ce45630674"
+    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-linux-x86_64.zip"
+    sha256 "4a31066d71363ee6e30e0882ef5a1335632d3155266f0f38764934f4520b0fb3"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ghokun/convert/releases/download/v0.0.2/convert-0.0.2-darwin-aarch64.zip"
-    sha256 "33364a9d2f4a94a7abb8d4f8e3131720b96dc61cc234f920eb69ad519c578a57"
+    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-darwin-aarch64.zip"
+    sha256 "fc02cd1270415cc50550486359e84ff158a7d7bdd3889aa9cbcb64604f7aa7a3"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ghokun/convert/releases/download/v0.0.2/convert-0.0.2-darwin-x86_64.zip"
-    sha256 "dde4d34876eca1f78ca308be309421eb00f1829d55feba5cc977c0b670186ad6"
+    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-darwin-x86_64.zip"
+    sha256 "4d2a4647d7f31bee9086103ce154c78d89b37bc15643ea3c2fa05f2bdb083647"
   end
 
 
@@ -30,6 +30,6 @@ class Convert < Formula
 
   test do
     output = shell_output("#{bin}/convert --version")
-    assert_match "0.0.2", output
+    assert_match "0.1.0", output
   end
 end
