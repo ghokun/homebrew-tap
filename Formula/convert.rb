0f1ba119d81757263341b5f7856703a1372207bf
jreleaserbot <jreleaser@kordamp.org>
2024-07-16 21:52:32 +0000

    convert v0.2.0

--- Formula/convert.rb
+++ Formula/convert.rb
@@ -1,25 +1,22 @@
-# Generated with JReleaser 1.6.0 at 2023-06-11T13:15:12.09825915Z
+# Generated with JReleaser 1.13.1 at 2024-07-16T21:52:23.419142683Z
+
 class Convert < Formula
   desc "Convert your data files"
   homepage "https://github.com/ghokun/convert"
-  version "0.1.0"
+  version "0.2.0"
   license "Apache-2.0"
 
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-linux-aarch64.zip"
-    sha256 "a406920eac20c92502a4a2b794f09dd946e6c555f866cf0144e046be443ec748"
-  end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-linux-x86_64.zip"
-    sha256 "4a31066d71363ee6e30e0882ef5a1335632d3155266f0f38764934f4520b0fb3"
+    url "https://github.com/ghokun/convert/releases/download/v0.2.0/convert-0.2.0-linux-x86_64.zip"
+    sha256 "b6692e4dcfaa2868c7a4f0f6678c3edc643f900c7a282b6623ab7ee196877133"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-darwin-aarch64.zip"
-    sha256 "fc02cd1270415cc50550486359e84ff158a7d7bdd3889aa9cbcb64604f7aa7a3"
+    url "https://github.com/ghokun/convert/releases/download/v0.2.0/convert-0.2.0-darwin-aarch64.zip"
+    sha256 "25852b95b068b61e0f71a7cf386c28b53fd7827392b9b10fdc3baeae548e71d5"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ghokun/convert/releases/download/v0.1.0/convert-0.1.0-darwin-x86_64.zip"
-    sha256 "4d2a4647d7f31bee9086103ce154c78d89b37bc15643ea3c2fa05f2bdb083647"
+    url "https://github.com/ghokun/convert/releases/download/v0.2.0/convert-0.2.0-darwin-x86_64.zip"
+    sha256 "a4734bd9570b7941906f869a89d7da91bd001a4bbf263f36ab67e082c2f59396"
   end
 
 
@@ -30,6 +27,6 @@ class Convert < Formula
 
   test do
     output = shell_output("#{bin}/convert --version")
-    assert_match "0.1.0", output
+    assert_match "0.2.0", output
   end
 end
